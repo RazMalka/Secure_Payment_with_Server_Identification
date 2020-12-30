2474cb2246eb5bada39c19c58492fe7ca9072486
Raz Malka <30453323+RazMalka@users.noreply.github.com>
2020-12-30 05:05:55 +0200

    #4

--- elliptic.py
+++ elliptic.py
@@ -4,27 +4,12 @@
 import collections
 
 def inv(n, q):
-    """div on PN modulo a/b mod q as a * inv(b, q) mod q
-    >>> assert n * inv(n, q) % q == 1
-    """
-    # n*inv % q = 1 => n*inv = q*m + 1 => n*inv + q*-m = 1
-    # => egcd(n, q) = (inv, -m, 1) => inv = egcd(n, q)[0] (mod q)
+    """div on PN modulo a/b mod q as a * inv(b, q) mod q """
     return egcd(n, q)[0] % q
-    #[ref] naive implementation
-    #for i in range(q):
-    #    if (n * i) % q == 1:
-    #        return i
-    #    pass
-    #assert False, "unreached"
-    #pass
-
 
 def egcd(a, b):
     """extended GCD
     returns: (s, t, gcd) as a*s + b*t == gcd
-    >>> s, t, gcd = egcd(a, b)
-    >>> assert a % gcd == 0 and b % gcd == 0
-    >>> assert a * s + b * t == gcd
     """
     s0, s1, t0, t1 = 1, 0, 0, 1
     while b > 0:
@@ -34,12 +19,8 @@ def egcd(a, b):
         pass
     return s0, t0, a
 
-
 def sqrt(ysq, q):
-    """sqrt on PN modulo: returns two numbers or exception if not exist
-    >>> assert (sqrt(n, q)[0] ** 2) % q == n
-    >>> assert (sqrt(n, q)[1] ** 2) % q == n
-    """
+    """sqrt on PN modulo: returns two numbers or exception if not exist"""
     assert ysq < q
     for i in range(1, q):
         if i * i % q == ysq:
@@ -47,9 +28,7 @@ def sqrt(ysq, q):
         pass
     raise Exception("not found")
 
-
 Coord = collections.namedtuple("Coord", ["x", "y"])
-
 
 class EC(object):
     """System of Elliptic Curve"""
@@ -77,11 +56,6 @@ class EC(object):
         """find points on curve at x
         - x: int < q
         - returns: ((x, y), (x,-y)) or not found exception
-        >>> a, ma = ec.at(x)
-        >>> assert a.x == ma.x and a.x == x
-        >>> assert a.x == ma.x and a.x == x
-        >>> assert ec.neg(a) == ma
-        >>> assert ec.is_valid(a) and ec.is_valid(ma)
         """
         assert x < self.q
         ysq = (x ** 3 + self.a * x + self.b) % self.q
@@ -89,20 +63,11 @@ class EC(object):
         return Coord(x, y), Coord(x, my)
 
     def neg(self, p):
-        """negate p
-        >>> assert ec.is_valid(ec.neg(p))
-        """
+        """negate p"""
         return Coord(p.x, -p.y % self.q)
 
     def add(self, p1, p2):
-        """<add> of elliptic curve: negate of 3rd cross point of (p1,p2) line
-        >>> d = ec.add(a, b)
-        >>> assert ec.is_valid(d)
-        >>> assert ec.add(d, ec.neg(b)) == a
-        >>> assert ec.add(a, ec.neg(a)) == ec.zero
-        >>> assert ec.add(a, b) == ec.add(b, a)
-        >>> assert ec.add(a, ec.add(b, c)) == ec.add(ec.add(a, b), c)
-        """
+        """<add> of elliptic curve: negate of 3rd cross point of (p1,p2) line"""
         if p1 == self.zero: return p2
         if p2 == self.zero: return p1
         if p1.x == p2.x and (p1.y != p2.y or p1.y == 0):
@@ -120,32 +85,20 @@ class EC(object):
         return Coord(x, y)
 
     def mul(self, p, n):
-        """n times <mul> of elliptic curve
-        >>> m = ec.mul(p, n)
-        >>> assert ec.is_valid(m)
-        >>> assert ec.mul(p, 0) == ec.zero
-        """
+        """n times <mul> of elliptic curve"""
         r = self.zero
         m2 = p
-        # O(log2(n)) add
+
         while 0 < n:
             if n & 1 == 1:
                 r = self.add(r, m2)
                 pass
             n, m2 = n >> 1, self.add(m2, m2)
             pass
-        # [ref] O(n) add
-        #for i in range(n):
-        #    r = self.add(r, p)
-        #    pass
         return r
 
     def order(self, g):
-        """order of point g
-        >>> o = ec.order(g)
-        >>> assert ec.is_valid(a) and ec.mul(a, o) == ec.zero
-        >>> assert o <= ec.q
-        """
+        """order of point g"""
         assert self.is_valid(g) and g != self.zero
         for i in range(1, self.q + 1):
             if self.mul(g, i) == self.zero:
